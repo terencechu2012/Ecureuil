b64dea4631eccd22cd5087514ccfe145e4df23d5
Terence Chu <terence.chu.2012@sis.smu.edu.sg>
2014-10-13 17:28:26 +0800

    second

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,4 +1,12 @@
 class GraphsController < ApplicationController
   def main
+    value = []
+    
+    for i in 1..25
+      value << {"x" => rand(30), 'y' => rand(30), 'size' => rand(10), 'shape' => 'circle'}
+    end
+    hash = {'key' => 'Comedy', 'values' => value}
+    @data = []
+    @data << hash
   end
 end
